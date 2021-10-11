627fb820ebb8049b220c60bdd57719a2a25d6d83
JCh6 <juanch@outlook.com>
2021-10-11 11:36:46 -0500

    add person document

---
+++ src/main/java/com/juanchaparro/opensearch/document/Person.java
@@ -0,0 +1,34 @@
+package com.juanchaparro.opensearch.document;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "person")
+public class Person {
+
+    @Id
+    @Field(type = FieldType.Keyword)
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
