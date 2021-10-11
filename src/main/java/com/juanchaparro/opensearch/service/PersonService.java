e61e5d62b4312deb68b6bcb0b1038679ae6ce70b
JCh6 <juanch@outlook.com>
2021-10-11 11:44:51 -0500

    add person service

---
+++ src/main/java/com/juanchaparro/opensearch/service/PersonService.java
@@ -0,0 +1,26 @@
+package com.juanchaparro.opensearch.service;
+
+import com.juanchaparro.opensearch.document.Person;
+import com.juanchaparro.opensearch.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final PersonRepository repository;
+
+    @Autowired
+    public PersonService(PersonRepository repository) {
+        this.repository = repository;
+    }
+
+    public void save(final Person person) {
+        repository.save(person);
+    }
+
+    public Person findById(final String id) {
+        return repository.findById(id).orElse(null);
+    }
+
+}
