6f30a477148e0fa4963adf886ed8eafe91898253
JCh6 <juanch@outlook.com>
2021-10-11 09:24:43 -0500

    first commit

---
+++ src/test/java/com/juanchaparro/opensearch/OpensearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanchaparro.opensearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpensearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
