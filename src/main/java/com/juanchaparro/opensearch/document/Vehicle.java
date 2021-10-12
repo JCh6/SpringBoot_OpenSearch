3ce1b7ccb5d1d6ce3ccb9e5ce49fb5911eeb21d6
JCh6 <juanch@outlook.com>
2021-10-12 08:53:20 -0500

    add Vehicle document

---
+++ src/main/java/com/juanchaparro/opensearch/document/Vehicle.java
@@ -0,0 +1,24 @@
+package com.juanchaparro.opensearch.document;
+
+public class Vehicle {
+
+    private String id;
+
+    private String number;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
