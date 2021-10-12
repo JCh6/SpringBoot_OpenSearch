cb54cededdd26fec4aa2567aa496b961f6a848c1
JCh6 <juanch@outlook.com>
2021-10-12 09:30:43 -0500

    add vehicleController

---
+++ src/main/java/com/juanchaparro/opensearch/controller/VehicleController.java
@@ -0,0 +1,29 @@
+package com.juanchaparro.opensearch.controller;
+
+import com.juanchaparro.opensearch.document.Vehicle;
+import com.juanchaparro.opensearch.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/vehicle")
+public class VehicleController {
+
+    private final VehicleService service;
+
+    @Autowired
+    public VehicleController(VehicleService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public void index(@RequestBody final Vehicle vehicle) {
+        service.index(vehicle);
+    }
+
+    @GetMapping("/{id}")
+    public Vehicle getById(@PathVariable final String id) {
+        return service.getById(id);
+    }
+
+}
