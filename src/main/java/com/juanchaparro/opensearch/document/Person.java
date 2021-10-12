9107eab4d839f5cb21a6dc293b9c780467135b9c
JCh6 <juanch@outlook.com>
2021-10-12 09:10:21 -0500

    add setting path for person

--- src/main/java/com/juanchaparro/opensearch/document/Person.java
+++ src/main/java/com/juanchaparro/opensearch/document/Person.java
@@ -5,8 +5,10 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
 
 @Document(indexName = Indices.PERSON_INDEX)
+@Setting(settingPath = "static/es-settings.json")
 public class Person {
 
     @Id
