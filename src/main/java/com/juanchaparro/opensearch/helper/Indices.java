adf828ef46fed68694ebb49889f3a67546325912
JCh6 <juanch@outlook.com>
2021-10-12 09:09:51 -0500

    add new index for vehicle

--- src/main/java/com/juanchaparro/opensearch/helper/Indices.java
+++ src/main/java/com/juanchaparro/opensearch/helper/Indices.java
@@ -3,5 +3,6 @@ package com.juanchaparro.opensearch.helper;
 public final class Indices {
 
     public static final String PERSON_INDEX = "person";
+    public static final String VEHICLE_INDEX = "vehicle";
 
 }
